e8e657f9957dd3ee430894e471f9e664ad16709f
Ukasz11233 <chmiel11233@gmail.com>
2023-04-16 17:28:53 +0200

    ignoring build directory

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <project/CurrentRates.h>
+#include <logger/Log.h>
+
 
 int main() {
     CurrentRates currentRates(20.4);
+    Log::LOG_DEBUG("test");
     currentRates.getCurrentRateFromFixerApi();
     return 0;
 }
